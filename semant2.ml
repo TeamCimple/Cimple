1219dec5f4fc338714652c993fcefa3908d4bb46
shankarapailoor <sp3485@columbia.edu>
2016-11-19 17:22:12 -0500

    checking that local variables do not have same name as globals

--- semant2.ml
+++ semant2.ml
@@ -24,6 +24,9 @@ let symbol_from_declaration = function
                                                                type_from_declaration_specifiers declspec)
    | _ -> raise(Failure("symbol_from_declaration: Unrecognized declaration"))    
 
+let get_decls_from_compound_stmt stmt = match stmt with 
+        CompoundStatement(x, y) -> x
+   | _ -> []
 
 let check_program program =
         let sdecls = List.map var_name_from_declaration program.globals in
@@ -35,12 +38,37 @@ let check_program program =
                 in helper (List.sort compare list)
         in 
         
-        report_duplicate (fun a -> "duplicate for variable: " ^ a)  (sdecls); 
+       report_duplicate (fun a -> "duplicate for variable: " ^ a)  (sdecls); 
 
        let fnames = List.map (fun func -> var_name_from_direct_declarator func.func_name) program.functions in
-
        report_duplicate (fun a -> "duplicate functions: " ^ a) (fnames);
 
        let has_main = List.mem "main" fnames in
+       if has_main then () else raise(Failure("no function main declared")); 
 
-       if has_main then () else raise(Failure("no function main declared")) 
+       (* Build map of function declarations *)
+       let functions_map = List.fold_left (fun m func -> StringMap.add
+       (var_name_from_direct_declarator func.func_name) func m) StringMap.empty
+       program.functions in
+
+       (* Build map of global declarations *)
+             let check_function func = 
+                let local_decls = List.map var_name_from_declaration
+                (get_decls_from_compound_stmt func.body) in
+
+                report_duplicate (fun a -> "duplicate local variable: " ^ a)
+                (local_decls);
+
+                let symbols = List.fold_left (fun m decl -> if StringMap.mem
+                (var_name_from_declaration decl) m then
+                        raise(Failure("redefining global")) else StringMap.add
+       (var_name_from_declaration decl) decl m) StringMap.empty
+       (program.globals @ (get_decls_from_compound_stmt func.body))
+       in
+ 
+
+                if StringMap.is_empty symbols then () else
+                        raise(Failure("sdfsdf"))
+
+        in List.iter check_function program.functions;
+
